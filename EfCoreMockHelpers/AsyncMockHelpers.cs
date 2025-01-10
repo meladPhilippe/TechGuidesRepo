83c7ca0538140b20c86457e60bd8e21d633d9394
Melad Boctor <melad.philippe@gmail.com>
2025-01-10 20:47:33 +0200

    Utility classes for mocking async Dbsets

---
+++ EfCoreMockHelpers/AsyncMockHelpers.cs
@@ -0,0 +1,96 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace EfCoreMockHelpers;
+
+public class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+{
+    public TestAsyncEnumerable(IEnumerable<T> enumerable)
+        : base(enumerable)
+    { }
+
+    public TestAsyncEnumerable(Expression expression)
+        : base(expression)
+    { }
+
+    public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+    {
+        return new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+    }
+
+    IQueryProvider IQueryable.Provider => new TestAsyncQueryProvider<T>(this);
+}
+public class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+{
+    private readonly IEnumerator<T> _inner;
+
+    public TestAsyncEnumerator(IEnumerator<T> inner)
+    {
+        _inner = inner;
+    }
+
+    public T Current => _inner.Current;
+
+    public ValueTask DisposeAsync()
+    {
+        _inner.Dispose();
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask<bool> MoveNextAsync()
+    {
+        return new ValueTask<bool>(_inner.MoveNext());
+    }
+}
+public class TestAsyncQueryProvider<T> : IAsyncQueryProvider
+{
+    private readonly IQueryProvider _inner;
+    public TestAsyncQueryProvider(IQueryProvider inner)
+    {
+        _inner = inner ?? throw new ArgumentNullException(nameof(inner));
+    }
+    public IQueryable CreateQuery(Expression expression)
+    {
+        if (expression == null)
+        {
+            throw new ArgumentNullException(nameof(expression));
+        }
+        return new TestAsyncEnumerable<T>(expression);
+    }
+    public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+    {
+        if (expression == null)
+        {
+            throw new ArgumentNullException(nameof(expression));
+        }
+
+        return new TestAsyncEnumerable<TElement>(expression);
+    }
+    public object Execute(Expression expression)
+    {
+        if (expression == null)
+        {
+            throw new ArgumentNullException(nameof(expression));
+        }
+
+        var result = _inner.Execute(expression);
+        return result ?? throw new InvalidOperationException("Execution returned null.");
+    }
+    public TResult Execute<TResult>(Expression expression)
+    {
+        return _inner.Execute<TResult>(expression);
+    }
+    public TResult ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (_inner is IAsyncQueryProvider asyncProvider)
+        {
+            return asyncProvider.ExecuteAsync<TResult>(expression, cancellationToken);
+        }
+        else
+        {
+            return _inner.Execute<TResult>(expression);
+        }
+    }
+}
---
+++ EfCoreMockHelpers/MockDbSet.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace EfCoreMockHelpers
+
+public static class MockDbSet
+{
+    public static Mock<DbSet<T>> Create<T>(IEnumerable<T> data) where T : class
+               => SetupMock(new Mock<DbSet<T>>(), data);
+    public static Mock<DbSet<T>> Setup<T>(Mock<DbSet<T>> mock, IEnumerable<T> data) where T : class
+               => SetupMock(mock, data);
+    private static Mock<DbSet<T>> SetupMock<T>(Mock<DbSet<T>> mock, IEnumerable<T> data) where T : class
+    {
+        var queryable = data.AsQueryable();
+        mock.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+        mock.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+        mock.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+        mock.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => queryable.GetEnumerator());
+
+        return mock;
+    }
+
+    public static Mock<DbSet<T>> Empty<T>() where T : class
+               => SetupMock(new Mock<DbSet<T>>(), Enumerable.Empty<T>());
+    public static Mock<DbSet<T>> CreateAsyncDbSet<T>(List<T> data) where T : class
+    {
+        var mockDbSet = new Mock<DbSet<T>>();
+
+        // Mock IAsyncEnumerable<T>
+        mockDbSet.As<IAsyncEnumerable<T>>()
+            .Setup(m => m.GetAsyncEnumerator(It.IsAny<CancellationToken>()))
+            .Returns(new TestAsyncEnumerator<T>(data.GetEnumerator()));
+
+        // Mock IQueryable<T>
+        mockDbSet.As<IQueryable<T>>()
+            .Setup(m => m.Provider)
+            .Returns(new TestAsyncQueryProvider<T>(data.AsQueryable().Provider));
+
+        mockDbSet.As<IQueryable<T>>()
+            .Setup(m => m.Expression)
+            .Returns(data.AsQueryable().Expression);
+
+        mockDbSet.As<IQueryable<T>>()
+            .Setup(m => m.ElementType)
+            .Returns(data.AsQueryable().ElementType);
+
+        mockDbSet.As<IQueryable<T>>()
+            .Setup(m => m.GetEnumerator())
+            .Returns(data.GetEnumerator());
+
+        return mockDbSet;
+    }
+
+}
